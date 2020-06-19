1178057ecb35519402f8f17cbf629db8bfbab6e4
saner2020-temp <otavio.lemos@unifesp.br>
2020-06-19 17:54:39 -0300

    feat: call youtube function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { youtube } = require('googleapis/build/src/apis/youtube')
+const runmanager = require('./youtube.js')
+
 require('./youtube.js')
 
 async function start() {
+  await runmanager()
 }
 
 start()
