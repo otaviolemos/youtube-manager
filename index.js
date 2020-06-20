0e2f599970a487ecb77d24a269615d945600ffde
Otavio Lemos <otaviolemos@gmail.com>
2020-06-20 10:20:44 -0300

    chore: clean up index

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const { youtube } = require('googleapis/build/src/apis/youtube')
 const runmanager = require('./youtube.js')
-
-require('./youtube.js')
 
 async function start() {
   await runmanager()
