e24b009f1c5555ae6604e86c0773807d3234a0b6
Otavio Lemos <otaviolemos@gmail.com>
2020-06-20 10:21:10 -0300

    chore: clean up youtube test

--- youtube.test.js
+++ youtube.test.js
@@ -2,5 +2,5 @@ const jsonPLFromName = require('./youtube.js')
 
 test('given a playlist name, creates the correct JSON object', () => {
   const jsonPL = jsonPLFromName('I1 - Metastudies')
-  expect(jsonPL.resource.snippet.title).toBe('I1 - Metastudies');
-});
+  expect(jsonPL.resource.snippet.title).toBe('I1 - Metastudies')
+})
