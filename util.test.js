e8e778021bda6be832464968d57c0ccc84f928ef
Otavio Lemos <otaviolemos@gmail.com>
2020-06-21 15:57:34 -0300

    test: util tests

---
+++ util.test.js
@@ -0,0 +1,13 @@
+const util = require('./util.js')
+
+
+test('get all lines from playlist names file', async () => {
+  const lines = await util.getAllNames()
+  expect(lines[0]).toBe('I1 - Metastudies')
+  expect(lines[59]).toBe('POSTERS')
+})
+
+test('given a playlist name, creates the correct JSON object', () => {
+  const jsonPL = util.jsonPLFromName('I1 - Metastudies')
+  expect(jsonPL.resource.snippet.title).toBe('I1 - Metastudies')
+})
