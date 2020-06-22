5a8b9b8d0644df4dce32e4cfe456ee7022128412
otaviolemos <otaviolemos@gmail.com>
2020-06-22 20:08:47 -0300

    Update util.js

--- util.js
+++ util.js
@@ -3,18 +3,14 @@ const readline = require('readline')
 
 async function getAllNames() {
   const fileStream = fs.createReadStream('resources/playlist-names.txt')
-
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
   })
-
   var names = []
-
   for await (const line of rl) {
     names.push(line)
   }
-
   return names
 }
 
