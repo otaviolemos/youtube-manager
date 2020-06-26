438e98499e04b8ce7567d0ff7e95390963cdf8ce
saner2020-temp <otavio.lemos@unifesp.br>
2020-06-26 16:02:53 -0300

    test: changed getAllNames function to getAllLines to be more generic

--- util.test.js
+++ util.test.js
@@ -1,11 +1,27 @@
 const util = require('./util.js')
+const fs = require('fs');
 
 
-test('get all lines from the playlist names file', async () => {
-  const lines = await util.getAllNames()
-  expect(lines[0]).toBe('I1 - Metastudies')
-  expect(lines[59]).toBe('POSTERS')
+test('get all lines from a file', async () => {
+  const data = new Uint8Array(Buffer.from('first name\nsecond name'));
+  createFile('names.txt', data)
+  const lines = await util.getAllLines('names.txt')
+  expect(lines[0]).toBe('first name')
+  expect(lines[1]).toBe('second name')
+  deleteFile('names.txt')
 })
+
+function createFile(name, data) { 
+  fs.writeFile(name, data, (err) => {
+    if (err) throw err('failed to create file');
+  }); 
+}
+
+function deleteFile(name) {
+  fs.unlink(name, function (err) {
+    if (err) throw err('failed to delete file');
+  }); 
+}
 
 test('given a playlist name, creates the correct JSON object', () => {
   const jsonPL = util.jsonPLFromName('I1 - Metastudies')
